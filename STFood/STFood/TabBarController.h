86532aacf09226ccb765b4796939a563b6f77c07
shenzhaoliang <409178030@qq.com>
2016-01-22 11:01:54 +0800

    添加podfile

---
+++ STFood/STFood/TabBarController.h
@@ -0,0 +1,13 @@
+//
+//  TabBarController.h
+//  STFood
+//
+//  Created by rkxt_ios on 16/1/22.
+//  Copyright © 2016年 ST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBarController : UITabBarController
+
+@end
---
+++ STFood/STFood/TabBarController.m
@@ -0,0 +1,27 @@
+//
+//  TabBarController.m
+//  STFood
+//
+//  Created by rkxt_ios on 16/1/22.
+//  Copyright © 2016年 ST. All rights reserved.
+//
+
+#import "TabBarController.h"
+
+@interface TabBarController ()
+
+@end
+
+@implementation TabBarController
+
+#pragma mark - --- lift cycle 生命周期 ---
+
+#pragma mark - --- delegate 视图委托 ---
+
+#pragma mark - --- event response 事件相应 ---
+
+#pragma mark - --- private methods 私有方法 ---
+
+#pragma mark - --- getters and setters 属性 ---
+
+@end
